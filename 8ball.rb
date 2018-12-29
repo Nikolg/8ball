6a1d9a4172e189c89ebb62930cd271c811dcafb7
Kos <ne-veshay-nos@mail.ru>
2018-12-29 03:02:14 +0300

    magic comments fixed

--- 8ball.rb
+++ 8ball.rb
@@ -1,6 +1,7 @@
-# Encoding UTF-8
-answers = File.readlines(__dir__ + "/" + "data/answers.txt", chomp: true)
-welcomes = File.readlines(__dir__ + "/" + "data/welcomes.txt", chomp: true)
+# encoding: utf-8
+
+answers = File.readlines(__dir__ + "/" + "data/answers.txt", chomp: true, encoding: "UTF-8")
+welcomes = File.readlines(__dir__ + "/" + "data/welcomes.txt", chomp: true, encoding: "UTF-8")
 
 puts welcomes.sample
 puts "Пожалуйста, подождите..."
